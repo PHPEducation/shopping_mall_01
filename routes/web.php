db868701b2755727967cfc6ef08c7de65a75d0d1
lxc150896 <lxc150896@gmail.com>
2018-09-27 16:56:26 +0700

    create template

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,18 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::group(['namespace'=>'Admin'],function(){
+	Route::group(['prefix'=>'login','middleware'=>'CheckLogedIn'],function(){
+		Route::get('/','LoginController@getLogin');
+		Route::post('/','LoginController@postLogin');		
+	});
+	Route::get('logout','HomeController@getLogout');
+	Route::group(['prefix'=>'admin','middleware'=>'CheckLogedOut'],function(){
+			Route::get('home','HomeController@getHome');
+
+			Route::group(['prefix'=>'category'],function(){
+				Route::get('/','CategoryController@getCate');
+				Route::get('edit','CategoryController@getEditCate');
+			});
+	});
+});
