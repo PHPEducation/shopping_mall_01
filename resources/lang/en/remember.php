cfc92f31afc540b170ffa01b15973723f9cb2b2b
lxc150896 <lxc150896@gmail.com>
2018-10-01 10:11:10 +0700

    login

---
+++ resources/lang/en/remember.php
@@ -0,0 +1,17 @@
+<?php
+return [
+    'Remember Me' => 'Remember Me!',
+    'Login' => 'Login',
+    'Administrator' => 'shop management',
+    'Logout' => 'Logout',
+    'Home' => 'Home page',
+    'Category' => 'Category',
+    'Product' => 'Product',
+    'Comment' => 'Comment',
+    'User' => 'User',
+    'Calendar' => 'Calendar',
+    'Error' => 'incorrect account or password!',
+    'Shop' => 'Shop Kim Cuong',
+    'Email' => 'E-mail',
+    'Pass' => 'Password',
+];
---
+++ resources/lang/vi/remember.php
@@ -0,0 +1,17 @@
+<?php
+return [
+    'Remember Me' => 'Lưu mật khẩu!',
+    'Login' => 'Đăng nhập',
+    'Administrator' => 'Quản trị shop',
+    'Logout' => 'Đăng xuất',
+    'Home' => 'Trang Chủ',
+    'Category' => 'Danh Mục',
+    'Product' => 'Sản Phẩm',
+    'Comment' => 'Bình Luận',
+    'User' => 'Người Dùng',
+    'Calendar' => 'Lịch',
+    'Error' => 'Tài khoản hoặc mật khẩu chưa đúng!',
+    'Shop' => 'Kim Cương Shop',
+    'Email' => 'E-mail',
+    'Pass' => 'Password',
+];
